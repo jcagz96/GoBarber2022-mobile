9c10bd6a5bf51671d6411466183d955e5d30bfe1
jcagz96 <jcagz@iscte-iul.pt>
2022-01-19 01:28:24 +0000

    SignUp page and navigation

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,65 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Image,
+  KeyboardAvoidingView,
+  ScrollView,
+  Platform,
+  View,
+  Keyboard,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+import logoImg from '../../assets/logo.png';
+import Button from '../../components/Button';
+import Input from '../../components/Input';
+import { RootStackParamList } from '../../routes';
 
-const SignUp: React.FC = () => <Container />;
+// const navigation = useNavigation<NativeStackNavigatorProps>();
 
+const SignUp: React.FC = () => {
+  const navigation =
+    useNavigation<NativeStackNavigationProp<RootStackParamList>>();
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      {...(Platform.OS === 'ios' ? { behavior: 'padding' } : {})}
+      enabled
+    >
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{ flex: 1 }}
+      >
+        <Container>
+          <Image source={logoImg} />
+          <View>
+            <Title>Crie a sua Conta</Title>
+          </View>
+
+          <Input name="name" icon="user" placeholder="Nome" />
+          <Input name="email" icon="mail" placeholder="E-mail" />
+          <Input name="password" icon="lock" placeholder="Senha" />
+          <Button
+            onPress={() => {
+              console.log('deu');
+            }}
+          >
+            Entrar
+          </Button>
+
+          <BackToSignIn
+            onPress={() => {
+              navigation.navigate('SignIn');
+            }}
+          >
+            <Icon name="arrow-left" size={20} color="#fff" />
+            <BackToSignInText>Voltar para logon</BackToSignInText>
+          </BackToSignIn>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+};
 export default SignUp;
